16b2bb41145f554eefd0a93c16192294e93120fd
aboutalvin <alvin86galang@gmail.com>
2024-12-12 21:17:30 +0700

    sj-rental

--- wp-content/themes/saudagarwp/assets/swiper/v8/retheme-swiper.minbfce.js
+++
@@ -1,1 +0,0 @@
-"use strict";function swiperjs(){document.querySelectorAll(".js-swiper").forEach(function(e,t){var n=JSON.parse(e.getAttribute("data-options")),r=e.querySelector(".swiper-button-prev"),s=e.querySelector(".swiper-button-next"),o=new Swiper(e,n);!1==n.navigation&&(r.style.display="none",s.style.display="none"),null!=e.querySelector(".wsb-countdown")&&o&&new countdown})}swiperjs();
